a9d17b7349699f4fde60590db66271cf72282c11
Vikas Vishwakarma <vikas.vishwakarma@smartports.se>
2025-09-19 21:28:32 +0530

    dockorrrrr

--- config/routes.rb
+++ config/routes.rb
@@ -3,26 +3,44 @@
 Rails.application.routes.draw do
   get 'comments/create'
   get 'comments/destroy'
-
-  devise_for :accounts, controllers: {
-    registrations: 'accounts/registrations'
-  }
+  devise_for :accounts
 
   get "/dashboard" => "accounts#index"
   get "show/:username" => "accounts#show", as: :show
 
-  resources :accounts, only: %i[index show edit update], param: :username do
+  resources :posts, only: %i[new create show destroy ]
+resources :accounts, only: %i[index show edit update], param: :username
+
+
+  resources :posts do
+    resources :likes, only: %i[create destroy]
+  end
+
+  resources :accounts do
     post "follow", to: "accounts#follow"
   end
 
-  resources :posts, only: %i[new create show destroy] do
-    resources :likes, only: %i[create destroy]
-    resources :comments, only: %i[create destroy] do
-      member do
-        get :toggle_replies
-      end
+# resources :accounts do
+#   resource :follow, only: [:create, :destroy] ,as: :follow
+# end
+
+# resources :posts do
+# end
+
+resources :posts do
+    resources :comments, only: [:create, :destroy]
+  resources :comments do
+    member do
+      get :toggle_replies
     end
   end
+end
+
+# resources :posts do
+#     post "comments", to: "comments#create"
+# end
+
+
 
   root "home#index"
 end
