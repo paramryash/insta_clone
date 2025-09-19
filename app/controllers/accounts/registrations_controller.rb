cd457f05cf701fd893938ec41d29c6b9d52b8d1a
Vikas Vishwakarma <vikas.vishwakarma@smartports.se>
2025-09-19 21:08:23 +0530

    dockorrr

--- app/controllers/accounts/registrations_controller.rb
+++ app/controllers/accounts/registrations_controller.rb
@@ -1,10 +1,7 @@
-module Accounts
-  class RegistrationsController < Devise::RegistrationsController
-    # Agar destroy action customize karna hai
-    def destroy
-      resource.destroy
-      Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-      redirect_to root_path, notice: "Account deleted successfully."
-    end
+class Accounts::RegistrationsController < Devise::RegistrationsController
+  def destroy
+    resource.destroy
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    redirect_to root_path, notice: "Account deleted successfully."
   end
 end
