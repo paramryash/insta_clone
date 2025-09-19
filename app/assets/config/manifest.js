ee44becfa0b96e1ab6f1c317c50f4bb51edc5776
Vikas Vishwakarma <vikas.vishwakarma@smartports.se>
2025-09-19 19:14:26 +0530

    delete accountt

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
-//= link controllers/application.js
 //= link controllers/comment_controller.js
 //= link controllers/index.js
